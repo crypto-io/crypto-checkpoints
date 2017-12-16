4858c6a77f5255fe73e0c2be02f0b32d3bac0d34
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-12-16 20:54:02 +0100

    :metal: Add CLI & remove default export

---
+++ bin/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const checkpoints = require('./../dist/checkpoints-node.js');
+
+checkpoints.start();
--- src/index.js
+++ src/index.js
@@ -1,47 +1,83 @@
 import CryptoDaemon from 'crypto-daemon';
 import { write } from 'crypto-io-utils';
 import { log, info, succes} from 'crypto-logger';
-const daemon = new CryptoDaemon();
 
-const startIPFS = exclusiveDaemon => {
-  log('updating checkpoints');
+export const daemon = new CryptoDaemon();
 
-  const IPFS = require('ipfs-api');
+export const startIPFS = () => {
+  return new Promise((resolve, reject) => {
+    log('starting ipfs');
+    try {
+      const IPFS = require('ipfs-api');
 
-  const ipfs = new IPFS({
-    PeerID: Math.random(),
-    EXPERIMENTAL: {
-      pubsub: true,
-    },
-  });
-
-  const set = [
-    {name: 'db', hash: 'QmRGE6LpXchjcZM5h6grF7cftqPUho5yw5Uya5M8qQF9KG'}
-  ]
-  set.map(hash => ipfs.name.resolve(hash));
-
-  info('resolving names');
-  let call = 0;
-  Promise.all(set.map(obj => {
-    call++;
-    info(`resolving ${call} of ${set.length}`)
-    async function run() {
-      const {Path} = await ipfs.name.resolve(obj.hash);
-      obj.hash = Path
-      return obj;
+      const ipfs = new IPFS({
+        PeerID: Math.random(),
+        EXPERIMENTAL: {
+          pubsub: true,
+        },
+      });
+      succes('starting ipfs');
+      resolve(ipfs);
+    } catch (error) {
+      fail('starting ipfs', error);
+      reject(error);
     }
-    return run();
-  })).then(json => {
-    log('writing checkpoints')
-    write('checkpoints.json', JSON.stringify(json)).then(() => {
-      succes('writing checkpoints');
-      succes('updating checkpoints');
-      if (exclusiveDaemon) daemon.stop();
-    });
   });
-
 };
 
-daemon.start()
-  .then(() => startIPFS(true))
-  .catch(error => {if (error.code === 'CRYPD_DAR') startIPFS(false)});
+export const updateCheckpoints = (ipfs, set) => {
+  log('updating checkpoints');
+  return new Promise((resolve, reject) => {
+    let call = 0;
+
+    if (!ipfs) {
+      fail('ipfs not found');
+      reject('ipfs not found');
+    }
+
+    set = set || [
+      {name: 'db', hash: 'QmRGE6LpXchjcZM5h6grF7cftqPUho5yw5Uya5M8qQF9KG'}
+    ];
+    try {
+      set.map(hash => ipfs.name.resolve(hash));
+
+      info('resolving names');
+      Promise.all(set.map(obj => {
+        call++;
+        info(`resolving ${call} of ${set.length}`)
+        async function run() {
+          const {Path} = await ipfs.name.resolve(obj.hash);
+          obj.hash = Path
+          return obj;
+        }
+        return run();
+      })).then(json => {
+        log('writing checkpoints')
+        write('checkpoints.json', JSON.stringify(json)).then(() => {
+          succes('writing checkpoints');
+          succes('updating checkpoints');
+          resolve();
+        });
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+export const start = () => {
+  async function run() {
+    try {
+      await daemon.start();
+      const ipfs = await startIPFS(true);
+      await updateCheckpoints(ipfs);
+      daemon.stop();
+    } catch (error) {
+      if (error.code === 'CRYPD_DAR') {
+        const ipfs = await startIPFS(true)
+        await updateCheckpoints(ipfs);
+      }
+    }
+  }
+  run();
+}
